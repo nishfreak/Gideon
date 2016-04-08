10db4a2f193e49b0c9dc2506b30f8adc14ef422f
Nishant Kambhatla <nish2493@gmail.com>
2016-04-08 20:17:04 +0530

    Adding more operations to the fileops unit

--- fileops.py
+++ fileops.py
@@ -1,7 +1,7 @@
 import os
 
 
-def current_directory():
+def current_dir():
     """
     This is a function to retrieve the current working directory.
     :rtype: string
@@ -19,15 +19,50 @@ def file_abs_path(file_name):
     return path
 
 
-def file_directory_exists(pathname):
+def file_dir_exists(path_name):
     """
-    This is a function to check if the specified path has the directory or filename.
+    This is a function to check if the specified path has the directory or file.
     :rtype: bool
     """
-    path = os.path.exists(pathname)
+    path = os.path.exists(path_name)
     return path
 
+
+def check_if_dir(path_name):
+    """
+    This is a function to check if the specified path belongs to a directory.
+    :rtype: bool
+    """
+    path = os.path.isdir(path_name)
+    return path
+
+
+def count_files(path_name):
+    count = len([name for name in os.listdir(path_name) if os.path.isfile(name)])
+    return count
+
+
+def list_dir_contents(path_name):
+    contents = os.listdir(path_name)
+    return contents
+
+
+def show_dir_tree(path_name):
+    for root, dirs, files in os.walk(path_name):
+        level = root.replace(path_name, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
 filename = "README.md"
-print current_directory()
+print current_dir()
 print file_abs_path(filename)
-print file_directory_exists(file_abs_path(filename))
+print file_dir_exists(file_abs_path(filename))
+print check_if_dir(current_dir())
+print count_files(current_dir())
+print list_dir_contents(current_dir())
+path = "/home/nishant/Documents/final project"
+show_dir_tree(path)
